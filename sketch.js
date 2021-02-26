b8858aaacbae63cd80d14a66fbf14cb6ba942429
adidevanilm <junaanilm@gmail.com>
2021-02-26 14:44:54 -0400

    slingshot

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
 var engine, world;
-var box1, pig1;
+var box1, pig1, pig3;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,6 +15,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
 
 
     ground = new Ground(600,height,1200,20);
@@ -33,17 +35,19 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230, 180, 80, PI/2);
 
     bird = new Bird(100,100);
 
+   slingshot = new Slingshot(bird.body, {x:200, y:100})    
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+ //   console.log(box2.body.position.x);
+  //  console.log(box2.body.position.y);
+  //  console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +64,15 @@ function draw(){
     log5.display();
 
     bird.display();
+
+   // log6.display();
+    slingshot.display();
+    
   //  platform.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(bird.body, { x:mouseX, y:mouseY })
+}
+function mouseReleased(){
+  slingshot.fly()
 }
